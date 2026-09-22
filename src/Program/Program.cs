770b4ba45ccf06b8e57c4537380ffc3291bbf817
Jorge Opizzo <aguopizzo@gmail.com>
2026-09-22 17:10:03 -0300

    Nuevamente corregimos el ejercicio debido a la reinterpretación del problema. Borramos las clase CarDatabase y MoviesCatalog y sus respectivos tests. Usamos la clase Repository<T> como base de datos genérica y probamos su efectividad ejecutando casos de uso en Program.cs. Ya no se usa herencia.

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -5,6 +5,7 @@
 //------------------------------------------------------------------------------
 
 using System;
+using Ucu.Poo.Repositories;
 
 namespace Ucu.Poo.Exercise
 {
@@ -18,7 +19,35 @@ namespace Ucu.Poo.Exercise
         /// </summary>
         public static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Repository<Car> repository = new Repository<Car>();
+            
+            Car car = new Car("Sandero", "Renault", 2015);
+            Car car2 = new Car("Uno", "Fiat", 2000);
+
+            repository.Add(car);
+            repository.Add(car2);
+
+            Car found1 = repository.Find(c => c.Model == "Sandero");
+            Car found2 = repository.Find(c => c.Model == "Uno");
+
+            Console.WriteLine($"{found1.Maker}");
+            Console.WriteLine($"{found2.Maker}");
+            
+
+            Repository<Movie> catalog = new Repository<Movie>();
+
+            Movie movie = new Movie("Avengers Endgame", 2018);
+            Movie movie2 = new Movie("The Avengers", 2014);
+
+            catalog.Add(movie);
+            catalog.Add(movie2);
+
+            Movie found3 = catalog.Find(c => c.Year == 2018);
+            Movie found4 = catalog.Find(c => c.Year == 2014);
+
+            Console.WriteLine($"{found3.Name}");
+            Console.WriteLine($"{found4.Name}");
+
         }
     }
 }
